b4084b3c43350182207460dd6eaf5d8f775e5f03
Mrbabet <wieliczkokamil@gmail.com>
2023-10-08 12:33:37 +0200

    initial commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  console.log(data);
+};
+const timeUpdate = function (data) {
+  console.log(data);
+};
+
+player.on('play', onPlay);
+player.on('timeupdate', timeUpdate);
