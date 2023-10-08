faef0caee34707aa63a8325ec20d1d6d886ec5b6
Mrbabet <wieliczkokamil@gmail.com>
2023-10-08 20:51:54 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,28 @@ import throttle from 'lodash.throttle';
 const email = document.querySelector('[name="email"]');
 const textarea = document.querySelector('[name="message"]');
 const form = document.querySelector('form');
+const FEEDBACK_DATA = 'feedback-form-state';
+let feedbackData = JSON.parse(localStorage.getItem(FEEDBACK_DATA)) || {};
+
+const getData = () => {
+  email.value = feedbackData.email || '';
+  textarea.value = feedbackData.message || '';
+};
+
+getData();
+
+const onSubmit = e => {
+  e.preventDefault();
+  console.log(feedbackData);
+
+  localStorage.removeItem(FEEDBACK_DATA);
+  e.currentTarget.reset();
+  feedbackData = {};
+};
+const updateFeedbackData = function (e) {
+  feedbackData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_DATA, JSON.stringify(feedbackData));
+};
+
+form.addEventListener('input', updateFeedbackData);
+form.addEventListener('submit', onSubmit);
